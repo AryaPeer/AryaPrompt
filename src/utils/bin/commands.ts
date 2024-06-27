026bc0eb7189a88e76b268cd5f2022d2c62af887
Arya Peer <arya.peer@gmail.com>
2024-06-27 02:34:20 +0000

    Removed whoami

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -70,10 +70,6 @@ export const echo = async (args: string[]): Promise<string> => {
   return args.join(' ');
 };
 
-export const whoami = async (args: string[]): Promise<string> => {
-  return `${config.ps1_username}`;
-};
-
 export const date = async (args: string[]): Promise<string> => {
   return new Date().toString();
 };
